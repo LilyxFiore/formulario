b0a5a57d873cfa07eeeb5cda43dc34b1e03821d5
LilyxFiore <liliana.fiorela@gmail.com>
2017-04-29 12:18:26 -0500

    subiendo validacion numero telefonicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,34 @@
 var element = document.querySelector(".content-form");
 element.addEventListener("submit", function(event) {
 
-
   var password = document.getElementById("input-password");
   var repassword = document.getElementById("input-repassword");
 
   if(password.value>repassword.value){
     event.preventDefault();
-    showToolTip("Contraseña de confirmación no coincide. Tiene menos carácteres que la contraseña",repassword); return false;
+    showToolTip("Contraseña de confirmación no coincide. Tiene menos carácteres que la contraseña",repassword);
+    return false;
   }
   else if(password.value<repassword.value)
   {
     event.preventDefault();
-    showToolTip("Contraseña de confirmación no coincide. Tiene más carácteres que la contraseña",repassword); return false;
+    showToolTip("Contraseña de confirmación no coincide. Tiene más carácteres que la contraseña",repassword);
+    return false;
   }
  else if (password.value===repassword.value) {
-    hiddenToolTip(repassword); return true;
- }
+    hiddenToolTip(repassword);
+    alert("Sus datos han sido registrados correctamente");
+    return true;
+  }
 });
 
-
+/**Ocultar toolTip **/
 function hiddenToolTip(nodoInput){
   var nodoSpan= nodoInput.nextElementSibling;
   nodoSpan.setAttribute("style","display:none");
 }
 
+/**Mostrar toolTip **/
 function showToolTip(texto,nodoInput){
     var nodoSpan= nodoInput.nextElementSibling;
     nodoSpan.setAttribute("style","display:block");
